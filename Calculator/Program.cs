d1e085a6a86e39dd9f9d7fa80923fea36135189a
jana62-jj <Maged_Mansour_Gamal_Ahmed-J@ulster.ac.uk>
2026-09-25 22:23:54 +0300

    updated program with if statments

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,13 +1,65 @@
-﻿Console.WriteLine("type the first number followed by the enter key");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿using System.ComponentModel.Design;
+using System.Xml.Serialization;
 
-Console.WriteLine("type the second number followed by the enter key");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-int result = firstNumber + secondNumber;
-
-Console.WriteLine("Adding {0} and {1} gives us {2}", firstNumber ,secondNumber, result);
+CalculatorApp();
+void CalculatorApp() { 
 
 
-//pauses the program waits until the user presses a key
-Console.ReadKey();
+    Console.WriteLine("type the first number followed by the enter key");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.WriteLine("type the second number followed by the enter key");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.WriteLine("choose an option from the followig list");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3- Divide");
+    Console.WriteLine("4 - Multiply");
+
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+
+        int result = firstNumber + secondNumber;
+
+        Console.WriteLine($"Adding {firstNumber}  and  {secondNumber} equals {result}");
+
+
+    }
+
+    else if (choice == 2)
+    {
+        int result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtractig {firstNumber} from {secondNumber} equals {result}");
+    }
+
+    else if (choice == 3)
+    {
+        int result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} from {secondNumber} equals {result}");
+    }
+
+    else if (choice == 4){
+        int result = firstNumber * secondNumber;
+        Console.WriteLine($" Multiply {firstNumber} from {secondNumber} equals {result}");
+
+    }
+
+    else
+    {
+        Console.WriteLine("Invalid choice");
+    }
+
+
+
+
+    }
+
+
+
+
+    
