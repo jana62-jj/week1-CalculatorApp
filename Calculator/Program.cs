8609f95ad683d9e9bba14f278a5b81bb80b535ea
jana62-jj <Maged_Mansour_Gamal_Ahmed-J@ulster.ac.uk>
2026-09-25 14:45:27 +0300

    Completed calculator input and addition

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("type the first number followed by the enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("type the second number followed by the enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives us {2}", firstNumber ,secondNumber, result);
+
+
+//pauses the program waits until the user presses a key
+Console.ReadKey();
